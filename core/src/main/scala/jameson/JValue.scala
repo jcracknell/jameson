4019fe463ce5c3c002970b15d8a535a0e55f8c47
James Cracknell <james.h.cracknell@gmail.com>
2016-08-11 23:01:39 -0600

    Hammer out scalar types

--- core/src/main/scala/jameson/JValue.scala
+++ core/src/main/scala/jameson/JValue.scala
@@ -1,5 +1,10 @@
 package jameson
 
+import jameson.util.IOUtil
+import java.io.{Reader, Writer}
+
+import scala.language.implicitConversions
+
 sealed trait JLookup {
   def isUndefined: Boolean
 
@@ -36,4 +41,100 @@ case object JUndefined extends JLookup {
 
 sealed trait JValue extends JLookup {
   def isUndefined: Boolean = false
+}
+
+sealed trait JScalar extends JValue {
+  /** Get the native representation of this scalar JSON value. */
+  def value: Any
+
+  def apply(name: String): JLookup = JUndefined
+  def apply(index: Int): JLookup = JUndefined
+}
+
+trait JScalarOf[+V] { _: JScalar =>
+  def value: V
+}
+
+object JScalarOf {
+  implicit def ordering[A](implicit vo: Ordering[A]): Ordering[JScalarOf[A]] = Ordering.by(_.value)
+}
+
+case object JNull extends JScalar with JScalarOf[Null] {
+  def value: Null = null
+}
+
+sealed trait JBoolean extends JScalar with JScalarOf[Boolean]
+
+object JBoolean {
+  def apply(value: Boolean): JBoolean = if(value) JTrue else JFalse
+  def unapply(b: JBoolean): Option[Boolean] = if(b == null) None else Some(b.value)
+}
+
+case object JTrue extends JBoolean {
+  def value: Boolean = true
+}
+
+case object JFalse extends JBoolean {
+  def value: Boolean = false
+}
+
+class JNumber(val value: Double) extends JScalar with JScalarOf[Double] {
+  def unary_- : JNumber = new JNumber(-value)
+  def + (r: JNumber.Repr): JNumber = new JNumber(this.value + r.value)
+  def - (r: JNumber.Repr): JNumber = new JNumber(this.value - r.value)
+  def * (r: JNumber.Repr): JNumber = new JNumber(this.value * r.value)
+  def / (r: JNumber.Repr): JNumber = new JNumber(this.value * r.value)
+  def % (r: JNumber.Repr): JNumber = new JNumber(this.value % r.value)
+
+  override def hashCode(): Int = value.hashCode()
+
+  override def equals(obj: Any): Boolean = obj match {
+    case that: JNumber => this.value == that.value
+    case _ => false
+  }
+
+  override def toString: String = s"JNumber($value)"
+}
+
+object JNumber {
+  def apply(repr: Repr): JNumber = new JNumber(repr.value)
+  def unapply(n: JNumber): Option[Double] = if(n == null) None else Some(n.value)
+
+  class Repr(val value: Double) extends AnyVal
+  object Repr {
+    implicit def ofJNumber(v: JNumber): Repr = new Repr(v.value)
+    implicit def ofDouble(v: Double): Repr = new Repr(v)
+    implicit def ofFloat(v: Float): Repr = new Repr(v.toDouble)
+    implicit def ofInt(v: Int): Repr = new Repr(v.toDouble)
+    implicit def ofShort(v: Short): Repr = new Repr(v.toDouble)
+  }
+}
+
+case class JString(value: String) extends JScalar with JScalarOf[String]
+
+object JString {
+  def encode(reader: Reader, writer: Writer): Unit = {
+    writer.write("\"")
+    using(new JStringWriter(writer)) { jsw =>
+      IOUtil.copy(reader, jsw)
+    }
+    writer.write("\"")
+  }
+
+  def encode(str: CharSequence, writer: Writer): Unit = {
+    writer.write("\"")
+    using(new JStringWriter(writer)) { jsw =>
+      jsw.write(str.toString)
+    }
+    writer.write("\"")
+  }
+
+  def encode(reader: Reader): String = {
+    val sw = new java.io.StringWriter
+    encode(reader, sw)
+    sw.toString
+  }
+
+  def encode(str: CharSequence): String =
+    encode(new java.io.StringReader(str.toString))
 }
