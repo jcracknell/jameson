f488d6d82fedaf7bb525e6d245b949a842e16b12
James Cracknell <james.h.cracknell@gmail.com>
2016-09-02 20:25:23 -0600

    Added encodeNumber test cases

--- core/src/test/scala/jameson/enc/EncodingJValueWriterSpec.scala
+++ core/src/test/scala/jameson/enc/EncodingJValueWriterSpec.scala
@@ -6,6 +6,28 @@ import org.scalatest.{FunSpec, Matchers}
 class EncodingJValueWriterSpec extends FunSpec with Matchers {
   import EncodingJValueWriter._
 
+  describe("encodeNumber") {
+    it("should encode integers") {
+      encodeNumber(0d) should be ("0")
+      encodeNumber(1d) should be ("1")
+      encodeNumber(-1d) should be ("-1")
+      encodeNumber(Int.MaxValue.toDouble) should be (Int.MaxValue.toString)
+      encodeNumber(Int.MinValue.toDouble) should be (Int.MinValue.toString)
+    }
+    it("should encode decimals") {
+      encodeNumber(1.23d) should be ("1.23")
+      encodeNumber(0.12d) should be ("0.12")
+      encodeNumber(0.0101d) should be ("0.0101")
+    }
+    it("should encode scientific notation") {
+      encodeNumber(1E20d) should be ("1E20")
+      encodeNumber(1.23E20d) should be ("1.23E20")
+      encodeNumber(1.0101E20) should be ("1.0101E20")
+      encodeNumber(1E-20d) should be ("1E-20")
+      encodeNumber(1.23E-20d) should be ("1.23E-20")
+      encodeNumber(1.0101E-20) should be ("1.0101E-20")
+    }
+  }
   describe("encodeString") {
     it("should escape quotes") {
       encodeString("\"") should be("\"\\\"\"")
